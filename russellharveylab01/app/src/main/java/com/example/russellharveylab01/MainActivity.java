e0753b53f0ff23fd3ec621260c15e23a94ab03bd
Russell <rdh1896@rit.edu>
2021-09-02 14:34:49 -0400

    finalized

--- russellharveylab01/app/src/main/java/com/example/russellharveylab01/MainActivity.java
+++ russellharveylab01/app/src/main/java/com/example/russellharveylab01/MainActivity.java
@@ -1,3 +1,9 @@
+/*
+Name: Russell Harvey
+Class: CSEC-467
+Project: Lab 01
+File: MainActivity.java
+ */
 package com.example.russellharveylab01;
 
 import android.content.Intent;
@@ -9,8 +15,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String SEC_ACT = "com.example.myfirstapp.SEC_ACT";
+    // Defining SEC_ACT variable for use in intent
+    public static final String SEC_ACT = "com.example.russellharveylab01.SecondActivity";
 
+    // Default onCreate() function
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +26,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View view) {
-        // EL BUTTON
+        /*
+        This function is used by the button with id:"@+id/button" as its "onClick" value.
+        This makes it so that when the button is clicked, this function will execute.
+         */
+        // Gets Intent here for second activity, uses this to define context
         Intent intent = new Intent(this, SecondActivity.class);
+        // Get EditText result
         EditText field = findViewById(R.id.plain_text_input);
         String result = field.getText().toString();
+        // Puts the output of the EditText field into the Intent
         intent.putExtra(SEC_ACT, result);
+        // Starts the new activity
         startActivity(intent);
     }
 }
--- russellharveylab01/app/src/main/java/com/example/russellharveylab01/SecondActivity.java
+++ russellharveylab01/app/src/main/java/com/example/russellharveylab01/SecondActivity.java
@@ -1,3 +1,10 @@
+/*
+Name: Russell Harvey
+Class: CSEC-467
+Project: Lab 01
+File: SecondActivity.java
+ */
+
 package com.example.russellharveylab01;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -5,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,23 +21,34 @@ public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        // Default onCreate() to load the activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
+        // Defines the message variable to get the extra content
         String message = intent.getStringExtra(MainActivity.SEC_ACT);
-
-        // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(message);
-
-        // TOAST
-        Toast.makeText(SecondActivity.this, message, Toast.LENGTH_LONG).show();
+        // Defines the toast button as a variable
+        Button b = (Button)findViewById(R.id.btntoast);
+        // Creates an OnClickListener and defines the onClick() function
+        View.OnClickListener o = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // FRESH, HOT-BUTTERED TOAST
+                Toast.makeText(SecondActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        };
+        // Sets "o" as the OnClickListener for "b"
+        b.setOnClickListener(o);
     }
 
     public void returnToMain(View view) {
+        /*
+        Not necessary, but similar to the "sendMessage()" method in MainActivity.java
+        this is tied to a button in activity_second.xml. This just allows you to return
+        to the main activity.
+         */
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
